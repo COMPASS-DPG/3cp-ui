cb1151fb83c0d007454f3820568db94a170874c0
Aman Agarwal <amanagarwal@Aditis-MacBook-Pro.local>
2023-12-14 14:42:27 +0530

    feat(dockerFile): updated docker file for build

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const courseManagerBackendUrl =
-  process.env.NEXT_PUBLIC_COURSE_MANAGER_BACKEND_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_COURSE_MANAGER_BACKEND_URL || 'http://localhost:4030';
 
 module.exports = {
   courseManagerBackendUrl,
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
 
   // Uncoment to add domain whitelist
   images: {
